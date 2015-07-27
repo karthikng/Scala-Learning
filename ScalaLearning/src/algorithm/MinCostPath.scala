f4c590171ab733edbc104ecd3f7a2c6f92044d97
karthik <ngkati@gmail.com>
2015-07-28 00:43:21 +0530

    Added minimum cost path algorithm implementation in Scala

--- ScalaLearning/src/algorithm/MinCostPath.scala
+++ ScalaLearning/src/algorithm/MinCostPath.scala
@@ -5,14 +5,16 @@ package algorithm
  */
 object MinCostPath {
   def minCostPath(cost: Array[Array[Int]], m: Int, n:Int) = {
-      var tc = Array.ofDim[Int](m, n)
+      var tc = Array.ofDim[Int](m+1, n+1)
+      
+      tc(0)(0) = cost(0)(0)
       
       for(i <- 1 to m) {
         tc(i)(0) = tc(i-1)(0) + cost(i)(0)
       }
       
       for(j <- 1 to n) {
-        tc(0)(j) = j;
+        tc(0)(j) = tc(0)(j-1) + cost(0)(j);
       }
        
       for(i <- 1 to m) {
@@ -21,15 +23,15 @@ object MinCostPath {
         }
       }
       
-      cost(m)(n)
+      tc(m)(n)
   }                                               
   
   def min(x:Int, y:Int, z:Int) : Int = {
     var min = x
-    if(y < x)  min = y
+    if(y < min) min = y
     if(z < min) min = z
-     min
-  }                                              
+    min
+  }                                               
   
   val cost = Array.ofDim[Int](3,3)                
                                                   
@@ -37,6 +39,6 @@ object MinCostPath {
   cost(1) = Array(4,8,2)
   cost(2) = Array(1,5,3)
   
-  println(minCostPath(cost, 2, 2))
+  println(minCostPath(cost, 2, 2))                
   
 }
